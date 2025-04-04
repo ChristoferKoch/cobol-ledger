7e410bb92e3fe7da2e67973623ff7d24de7bd870
Christofer Koch <cbkoch13@gmail.com>
2025-04-04 14:51:21 -0700

    feat: add basic structure to cobol_ledger.cob

---
+++ cobol_ledger.cob
@@ -0,0 +1,14 @@
+       IDENTIFICATION DIVISION.
+       PROGRAM-ID. CobolLedger.
+       AUTHOR. Christofer Koch.
+
+       ENVIRONMENT DIVISION.
+       FILE-CONTROL.
+
+       DATA DIVISION.
+       FILE SECTION.
+       WORKING-STORAGE SECTION.
+       REPORT SECTION.
+
+       PROCEDURE DIVISION.
+                                        
