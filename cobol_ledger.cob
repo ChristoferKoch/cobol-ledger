e21d2b4e8d23b79e26d4030232b4970382f2aee3
Christofer Koch <cbkoch13@gmail.com>
2025-04-11 12:28:10 -0700

    feat: add command line processing

--- cobol_ledger.cob
+++ cobol_ledger.cob
@@ -3,26 +3,69 @@
        AUTHOR. Christofer Koch.
 
        ENVIRONMENT DIVISION.
+       INPUT-OUTPUT SECTION.
        FILE-CONTROL.
            SELECT LedgerFile ASSIGN TO LedgerFileName
-               ORGANIZATION IS SEQUENTIAL.
+               ORGANIZATION IS LINE SEQUENTIAL.
        DATA DIVISION.
        FILE SECTION.
        FD LedgerFile.
        01 LedgerRecord.
-           02 TransactionDate      PIC X(5) VALUE SPACES.
-           02 TransactionStatus    PIC X VALUE SPACE.
-           02 TransactionCode      PIC X(10) VALUE SPACES.
-           02 TransactionDesc      PIC X(30) VALUE SPACES.
+           02 TransactionDate      PIC X(5).
+           02 TransactionStatus    PIC X.
+           02 TransactionCode      PIC X(10).
+           02 TransactionDesc      PIC X(30).
            02 TransactionLines.
-               03 GeneraAccount    PIC X(10) VALUE SPACES.
-               03 SpeciesAccount   PIC X(10) VALUE SPACES.
-               03 SpecificAccount  PIC X(10) VALUE SPACES.
-               03 LineAmount       PIC $$$,$$$,$$$,$$$.99 VALUE ZEROES.
+               03 GeneraAccount    PIC X(10).
+               03 SpeciesAccount   PIC X(10).
+               03 SpecificAccount  PIC X(20).
+               03 LineAmount       PIC 9(9)V99.
        WORKING-STORAGE SECTION.
-       01 LedgerFileName   PIC X(30) VALUE SPACES.
+       01 CommandLineArgs  PIC X(30).
+       01 NumOfArgs        PIC 99.
+       01 LedgerFileName   PIC X(30).
+       01 ReportType       PIC X(10).
+       01 Filters          PIC X(50).
+       01 FilterPtr        PIC 99 VALUE 1.
+       01 ReportTotal      PIC $$$,$$$,$$9.99
        REPORT SECTION.
-       MOVE   TO LedgerFileName
+
 
        PROCEDURE DIVISION.
-                                        
+       Main.
+           PERFORM GetCommandLineArgs
+
+           OPEN INPUT LedgerFile
+
+           CLOSE LedgerFile
+                   
+           STOP RUN.
+
+       GetCommandLineArgs.
+           ACCEPT NumOfArgs
+               FROM ARGUMENT-NUMBER
+           END-ACCEPT
+
+           SUBTRACT 1 FROM NumOfArgs
+
+           PERFORM NumOfArgs TIMES
+           ACCEPT CommandLineArgs
+               FROM ARGUMENT-VALUE
+           END-ACCEPT
+               
+           EVALUATE CommandLineArgs
+               WHEN "-f" ACCEPT CommandLineArgs
+                       FROM ARGUMENT-VALUE
+                       MOVE CommandLineArgs TO LedgerFileName
+               WHEN "balance"
+                   MOVE CommandLineArgs TO ReportType
+               WHEN "register"
+                   MOVE CommandLineArgs TO ReportType
+               WHEN "cleared"
+                   MOVE CommandLineArgs TO ReportType
+               WHEN OTHER STRING CommandLineArgs   DELIMITED BY SPACES
+                       ","                         DELIMITED BY SIZE
+                       INTO Filters WITH POINTER FilterPtr
+           END-EVALUATE
+           END-PERFORM.
+                      
