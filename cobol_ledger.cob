a3372a5db854aec3d8f540de4611e5c5af411a60
Christofer Koch <cbkoch13@gmail.com>
2025-04-13 12:29:43 -0700

    feat: add report files

--- cobol_ledger.cob
+++ cobol_ledger.cob
@@ -7,10 +7,13 @@
        FILE-CONTROL.
            SELECT LedgerFile ASSIGN TO LedgerFileName
                ORGANIZATION IS LINE SEQUENTIAL.
+           SELECT LedgerReport ASSIGN TO ReportName
+               ORGANIZATION IS SEQUENTIAL.
        DATA DIVISION.
        FILE SECTION.
        FD LedgerFile.
-       01 LedgerRecord.
+       01 RecordLine.
+           88 EndOfFile            VALUE HIGH-VALUES.
            02 TransactionDate      PIC X(5).
            02 TransactionStatus    PIC X.
            02 TransactionCode      PIC X(10).
@@ -20,6 +23,10 @@
                03 SpeciesAccount   PIC X(10).
                03 SpecificAccount  PIC X(20).
                03 LineAmount       PIC 9(9)V99.
+                                        
+       FD LedgerReport.
+       01 PrintLine                PIC X(50).
+
        WORKING-STORAGE SECTION.
        01 CommandLineArgs  PIC X(30).
        01 NumOfArgs        PIC 99.
@@ -27,7 +34,9 @@
        01 ReportType       PIC X(10).
        01 Filters          PIC X(50).
        01 FilterPtr        PIC 99 VALUE 1.
-       01 ReportTotal      PIC $$$,$$$,$$9.99
+       01 ReportName       PIC X(30).
+       01 CurrentDate      PIC X(8).
+       01 ReportTotal      PIC $$$,$$$,$$9.99.
        REPORT SECTION.
 
 
@@ -37,7 +46,25 @@
 
            OPEN INPUT LedgerFile
 
-           CLOSE LedgerFile
+           ACCEPT CurrentDate FROM DATE YYYYMMDD
+
+           STRING ReportType DELIMITED BY SPACES
+               "_report_"
+               CurrentDate
+               ".rpt"
+               INTO ReportName
+           END-STRING
+           DISPLAY ReportName
+
+           OPEN OUTPUT LedgerReport
+
+           EVALUATE ReportType
+               WHEN "balance" PERFORM GenBalanceReport
+               WHEN "register" PERFORM GenRegisterReport
+               WHEN "cleared" PERFORM GenClearedReport
+           END-EVALUATE
+
+           CLOSE LedgerFile, LedgerReport
                    
            STOP RUN.
 
@@ -68,4 +95,10 @@
                        INTO Filters WITH POINTER FilterPtr
            END-EVALUATE
            END-PERFORM.
+
+       GenBalanceReport.
+
+       GenRegisterReport.
+
+       GenClearedReport.
                       
