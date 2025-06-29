5e1a243aded6ee18630b66ae202d83968e69aeb7
Christofer Koch <cbkoch13@gmail.com>
2025-06-29 05:26:04 -0700

    feat: created stored tables

--- balance.cob
+++ balance.cob
@@ -1,22 +1,100 @@
        IDENTIFICATION DIVISION.
-       PROGRAM-ID. BalanceReport IS INITIAL.
+       PROGRAM-ID. BalanceReport.
 
        ENVIRONMENT DIVISION.
        INPUT-OUTPUT SECTION.
        FILE-CONTROL.
-           SELECT SORTEDDATA ASSIGN TO "BalanceReport.tmp"
+           SELECT LEDGER-FILE ASSIGN TO LEDGER-FILE-NAME
+               ORGANIZATION IS LINE SEQUENTIAL.
+           SELECT LEDGER-REPORT ASSIGN TO REPORT-NAME
                ORGANIZATION IS SEQUENTIAL.
 
        DATA DIVISION.
+       FILE SECTION.
+       FD LEDGER-FILE.
+       01  RECORD-LINE.
+           88 END-OF-FILE          VALUE HIGH-VALUES.
+           05 DETAIL-LINE          PIC X(100).
+       FD  LEDGER-REPORT.
+       01  PRINT-LINE              PIC X(50).
+                                        
        WORKING-STORAGE SECTION.
+       01  REPORT-TYPE             PIC X(10).
+       01  INFO-LINE 		       PIC X VALUE 'N'.
+       01  BALANCE-INDEX	       PIC 9999 VALUE 1.
+       01  LINE-COUNT              PIC 99 VALUE ZEROES.
+       01  TMP-ACCOUNTS.
+           05  TMP-GENERA	       PIC X(20).
+           05  TMP-SPECIES	       PIC X(20).
+	       05  TMP-INDIVIDUAL      PIC X(40).
+           05  TMP-LINE-TOTAL      PIC 9(9)V99.
+       01  BALANCE-LINE-TABLE.
+           05  BALANCE-LINE        OCCURS 1 TO 1000 TIMES
+                                        DEPENDING ON LINE-COUNT.
+               10  GENERA-ACCT	   PIC X(20).
+               10  SPECIES-ACCT    PIC X(20).
+               10  INDIVIDUAL-ACCT PIC X(40).
+               10  LINE-TOTAL	   PIC 9(9)V99.
 
-       PROCEDURE DIVISION USING LEDGERFILE, LEDGERREPORT, RECORDLINE,
-               TRANSACTIONLINE, CURRENTDATE, FILTERS.
+       LINKAGE SECTION.
+       01  NUM-OF-ARGS             PIC 99.
+       01  LEDGER-FILE-NAME        PIC X(30).
+       01  FILTER-TABLE.
+           05  FILTERS             PIC X(30) OCCURS 1 TO 10 TIMES
+                                        DEPENDING ON NUM-OF-ARGS
+                                        INDEXED BY FILTER-IDX.
+       01  FILTER-PTR              PIC 99.
+       01  REPORT-NAME             PIC X(30).
+       01  THIS-DATE               PIC X(8).
+       
+
+       PROCEDURE DIVISION USING NUM-OF-ARGS, LEDGER-FILE-NAME,
+               FILTER-TABLE, FILTER-PTR, REPORT-NAME, THIS-DATE.    
        0200-BALANCE-MAIN.
-           SORT SORTEDDATA ON 
+           OPEN INPUT LEDGER-FILE, OUTPUT LEDGER-REPORT
+           PERFORM 0210-GENERATE-TABLE
+           SORT BALANCE-LINE ON ASCENDING KEY GENERA-ACCT
+               SPECIES-ACCT
+               INDIVIDUAL-ACCT
+           DISPLAY BALANCE-LINE-TABLE
 
+           CLOSE LEDGER-FILE, LEDGER-REPORT
            EXIT PROGRAM.
 
-       0210-SORT-DATA.
-               
-               
+       0210-GENERATE-TABLE.
+           READ LEDGER-FILE
+               AT END SET END-OF-FILE TO TRUE
+           END-READ
+
+           PERFORM UNTIL END-OF-FILE
+               IF DETAIL-LINE FUNCTION CHAR(1) = " " AND
+                   DETAIL-LINE FUNCTION CHAR(2) = " " THEN
+               UNSTRING DETAIL-LINE DELIMITED BY ":" OR "  "
+                   INTO TMP-GENERA, TMP-SPECIES,
+                   TMP-INDIVIDUAL, TMP-LINE-TOTAL
+               END-UNSTRING
+               IF FILTER-PTR > 1 THEN
+                   SEARCH FILTERS
+                       WHEN TMP-GENERA = FILTERS(FILTER-IDX) OR
+                             TMP-SPECIES = FILTERS(FILTER-IDX) OR
+                             TMP-INDIVIDUAL = FILTERS(FILTER-IDX)
+                           MOVE "Y" TO INFO-LINE
+                   END-SEARCH
+               ELSE
+                   MOVE "Y" TO INFO-LINE
+               END-IF
+               IF INFO-LINE = "Y" THEN
+                   MOVE TMP-GENERA TO GENERA-ACCT(BALANCE-INDEX)
+                   MOVE TMP-SPECIES TO SPECIES-ACCT(BALANCE-INDEX)
+                   MOVE TMP-INDIVIDUAL TO INDIVIDUAL-ACCT(BALANCE-INDEX)
+                   MOVE TMP-LINE-TOTAL TO LINE-TOTAL(BALANCE-INDEX)
+                   ADD 1 TO BALANCE-INDEX
+                   ADD 1 TO LINE-COUNT
+               END-IF
+               END-IF
+               MOVE "N" TO INFO-LINE
+               READ LEDGER-FILE
+                   AT END SET END-OF-FILE TO TRUE
+               END-READ
+           END-PERFORM.    
+                      
--- cobol_ledger.cob
+++ cobol_ledger.cob
@@ -2,131 +2,78 @@
        PROGRAM-ID. CobolLedger.
        AUTHOR. Christofer Koch.
        DATE-COMPILED.
-
-       ENVIRONMENT DIVISION.
-       INPUT-OUTPUT SECTION.
-       FILE-CONTROL.
-           SELECT LEDGERFILE ASSIGN TO LEDGERFILENAME
-               ORGANIZATION IS LINE SEQUENTIAL.
-           SELECT LEDGERREPORT ASSIGN TO REPORTNAME
-               ORGANIZATION IS SEQUENTIAL.
                
        DATA DIVISION.
-       FILE SECTION.
-       FD LEDGERFILE.
-       01 RECORDLINE.
-           88 ENDOFFILE            VALUE HIGH-VALUES.
-           05 TYPECODE             PIC X.
-               88 COMMENT          VALUE ";".
-               88 DATELINE         PIC 9.
-               88 TRANSACTIONLINE  VALUE " ".
-           05 TRANSACTIONDATE      PIC X(5).
-           05 TRANSACTIONSTATUS    PIC X.
-           05 TRANSACTIONCODE      PIC X(10).
-           05 TRANSACTIONDESC      PIC X(30).
-
-       01 TRANSACTIONLINES.
-           05 TYPECODE             PIC X.
-           05 GENERAACCOUNT        PIC X(10).
-           05 SPECIESACCOUNT       PIC X(10).
-           05 SPECIFICACCOUNT      PIC X(20).
-           05 LINEAMOUNT           PIC 9(9)V99.
-                                        
-       FD LEDGERREPORT.
-       01 PRINTLINE                PIC X(50).
-
        WORKING-STORAGE SECTION.
-       01 COMMANDLINEARGS          PIC X(30).
-       01 NUMOFARGS                PIC 99.
-       01 LEDGERFILENAME           PIC X(30).
-       01 REPORTTYPE               PIC X(10).
-       01 PRINTREPORT              PIC X VALUE "F".
-       01 FILTERS                  PIC X(50).
-       01 FILTERPTR                PIC 99 VALUE 1.
-       01 REPORTNAME               PIC X(30).
-       01 CURRENTDATE              PIC X(8).
-       01 REPORTTOTAL              PIC $$$,$$$,$$9.99.
-       REPORT SECTION.
-
+       01  COMMAND-LINE-ARGS   PIC X(30).
+       01  NUM-OF-ARGS         PIC 99.
+       01  LEDGER-FILE-NAME    PIC X(30).
+       01  REPORT-TYPE         PIC X(10).
+       01  FILTER-TABLE.
+           05  FILTERS         PIC X(30) OCCURS 1 TO 10 TIMES
+                                        DEPENDING ON NUM-OF-ARGS
+                                        INDEXED BY FILTER-IDX.
+       01  FILTER-PTR          PIC 99 VALUE 1.
+       01  REPORT-NAME         PIC X(30).
+       01  THIS-DATE           PIC X(8).
 
        PROCEDURE DIVISION.
        0000-MAIN.
-           PERFORM 0100-GETCOMMANDLINEARGS
-               
-           ACCEPT CURRENTDATE FROM DATE YYYYMMDD
+           PERFORM 0100-GET-COMMAND-LINE-ARGS
 
-           STRING REPORTTYPE DELIMITED BY SPACES
+           DISPLAY FILTER-TABLE
+               
+           ACCEPT THIS-DATE FROM DATE YYYYMMDD
+
+           STRING REPORT-TYPE DELIMITED BY SPACES
                "_report_"
-               CURRENTDATE
+               THIS-DATE
                ".rpt"
-               INTO REPORTNAME
+               INTO REPORT-NAME
            END-STRING
 
-           EVALUATE REPORTTYPE
+           EVALUATE REPORT-TYPE
                WHEN "balance"
-                   CALL "BalanceReport" USING BY REFERENCE LEDGERFILE,
-                       LEDGERREPORT, RECORDLINE, TRANSACTIONLINE
-                       BY CONTENT CURRENTDATE, FILTERS                              
+                   CALL "BalanceReport" USING BY CONTENT NUM-OF-ARGS
+                       LEDGER-FILE-NAME
+                       FILTER-TABLE
+                       FILTER-PTR
+                       REPORT-NAME
+                       FILTER-IDX
+                       THIS-DATE
                WHEN "register"
-                   CALL "RegisterReport" USING BY REFERENCE LEDGERFILE,
-                       LEDGERREPORT, RECORDLINE, TRANSACTIONLINE
-                       BY CONTENT CURRENTDATE, FILTERS   
+                   CALL "RegisterReport"
                WHEN "cleared"
-                   CALL "ClearedReport" USING BY REFERENCE LEDGERFILE,
-                       LEDGERREPORT, RECORDLINE, TRANSACTIONLINE
-                       BY CONTENT CURRENTDATE, FILTERS   
+                   CALL "ClearedReport"
                WHEN OTHER DISPLAY "Invalid Report Type Given"
            END-EVALUATE.
 
-           CLOSE LEDGERFILE, LEDGERREPORT.
-                   
            STOP RUN.
 
-       0100-GETCOMMANDLINEARGS.
-           ACCEPT NUMOFARGS
+       0100-GET-COMMAND-LINE-ARGS.
+           ACCEPT NUM-OF-ARGS
                FROM ARGUMENT-NUMBER
            END-ACCEPT
 
-           SUBTRACT 1 FROM NUMOFARGS
+           SUBTRACT 1 FROM NUM-OF-ARGS
 
-           PERFORM NUMOFARGS TIMES
-           ACCEPT COMMANDLINEARGS
+           PERFORM NUM-OF-ARGS TIMES
+           ACCEPT COMMAND-LINE-ARGS
                FROM ARGUMENT-VALUE
            END-ACCEPT
                
-           EVALUATE COMMANDLINEARGS
-               WHEN "-f" ACCEPT COMMANDLINEARGS
+           EVALUATE COMMAND-LINE-ARGS
+               WHEN "-f" ACCEPT COMMAND-LINE-ARGS
                        FROM ARGUMENT-VALUE
-                       MOVE COMMANDLINEARGS TO LEDGERFILENAME
+                       MOVE COMMAND-LINE-ARGS TO LEDGER-FILE-NAME
                WHEN "balance"
-                   MOVE COMMANDLINEARGS TO REPORTTYPE
+                   MOVE COMMAND-LINE-ARGS TO REPORT-TYPE
                WHEN "register"
-                   MOVE COMMANDLINEARGS TO REPORTTYPE
+                   MOVE COMMAND-LINE-ARGS TO REPORT-TYPE
                WHEN "cleared"
-                   MOVE COMMANDLINEARGS TO REPORTTYPE
-               WHEN "-print"
-                   MOVE "T" TO PRINTREPORT
-               WHEN OTHER STRING COMMANDLINEARGS   DELIMITED BY SPACES
-                       ","                         DELIMITED BY SIZE
-                       INTO FILTERS WITH POINTER FILTERPTR
+                   MOVE COMMAND-LINE-ARGS TO REPORT-TYPE
+               WHEN OTHER MOVE COMMAND-LINE-ARGS TO FILTERS(FILTER-PTR)
+                       ADD 1 TO FILTER-PTR
            END-EVALUATE
            END-PERFORM.
-
-       0200-GENBALANCEREPORT.                       
-           GENERATE DETAILLINE
-           READ LEDGERFILE
-               AT END SET ENDOFFILE TO TRUE
-           END-READ.
-
-       0300-GENREGISTERREPORT.
-           GENERATE DETAILLINE
-           READ LEDGERFILE
-               AT END SET ENDOFFILE TO TRUE
-           END-READ.
-               
-       0400-GENCLEAREDREPORT.
-           GENERATE DETAILLINE
-           READ LEDGERFILE
-               AT END SET ENDOFFILE TO TRUE
-           END-READ.
                       
